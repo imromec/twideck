3798d38330c2e3f3b6447a3396b3ecd3d55216f2
meghraj.suthar <meghraj.suthar@qriyo.com>
2016-09-11 17:22:21 +0530

    udpated code

--- includes/global.php
+++ includes/global.php
@@ -3,8 +3,8 @@ session_start();
 
 define('CONSUMER_KEY', 'vcv0tvp673euCPav9a4ncdMnS');
 define('CONSUMER_SECRET', 'gfObR3PbFuzFczhuMmf6o5vrMsonvnz6WyAFnghviXNP6CAzQq');
-//define('OAUTH_CALLBACK', 'https://twideck.herokuapp.com/process/');
-define('OAUTH_CALLBACK', 'http://localhost/twideck/process/');
+define('OAUTH_CALLBACK', 'https://twideck.herokuapp.com/process/');
+//define('OAUTH_CALLBACK', 'http://localhost/twideck/process/');
 
 require_once 'classes/activity_class.php';
  
