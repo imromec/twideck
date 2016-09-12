3f9c2d23be8730c718320d7f934d3b0a8ecfb568
meghraj.suthar <meghraj.suthar@qriyo.com>
2016-09-12 23:30:50 +0530

    udpate cdode

--- header.php
+++ header.php
@@ -23,6 +23,7 @@ $userProfileImage = str_replace('_normal', '_bigger', $userProfileImage);
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>twideck - Simplify Your Twitter Experience</title>
 
+	<link rel="icon" href="/img/favicon.png" type="image/png">
 	<!-- Latest compiled and minified bootstrap framework -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
