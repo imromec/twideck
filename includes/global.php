6b781d8c9b2abb24376e4080f821cd189a43a746
Meghraj Suthar <meghrajsuthar03@gmail.com>
2016-09-14 15:17:01 +0530

    Update global.php

--- includes/global.php
+++ includes/global.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-define('CONSUMER_KEY', 'vcv0tvp673euCPav9a4ncdMnS');
-define('CONSUMER_SECRET', 'gfObR3PbFuzFczhuMmf6o5vrMsonvnz6WyAFnghviXNP6CAzQq');
+define('CONSUMER_KEY', '****');
+define('CONSUMER_SECRET', '******');
 define('OAUTH_CALLBACK', 'https://twideck.herokuapp.com/process/');
 //define('OAUTH_CALLBACK', 'http://127.0.0.1/twideck/process/');
 
